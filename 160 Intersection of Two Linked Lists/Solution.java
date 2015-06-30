9d7a7f40f35e0a102293147b45fb827bb83554d9
Dejun Huang <djhuang21@gmail.com>
2015-06-29 22:58:03 -0400

    Add solution for 160 Intersection of Two Linked Lists

---
+++ 160 Intersection of Two Linked Lists/Solution.java
@@ -0,0 +1,96 @@
+/*
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+
+For example, the following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗            
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+
+
+Notes:
+
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+Your code should preferably run in O(n) time and use only O(1) memory.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+
+        if (tempA == null || tempB == null) {
+        	return null;
+        }
+
+        int lenA = 1;
+        int lenB = 1;
+
+        while (tempA.next != null) {
+        	tempA = tempA.next;
+        	lenA ++;
+        }
+
+        while (tempB.next != null) {
+        	tempB = tempB.next;
+        	lenB ++;
+        }
+
+        if (tempA != tempB) {
+        	return null;
+        }
+
+		tempA = headA;
+        tempB = headB;
+
+        if (lenA >= lenB) {
+        	int distance = lenA - lenB;
+
+        	while (distance > 0) {
+        		tempA = tempA.next;
+        		distance --;
+        	}
+
+        	while (tempA != null && tempB != null && tempA != tempB) {
+        		tempA = tempA.next;
+        		tempB = tempB.next;
+        	}
+
+        	return tempA;
+        }
+        //lenA < lenB
+        else {
+        	int distance = lenB - lenA;
+
+        	while (distance > 0) {
+        		tempB = tempB.next;
+        		distance --;
+        	}
+
+        	while (tempA != null && tempB != null && tempA != tempB) {
+        		tempA = tempA.next;
+        		tempB = tempB.next;
+        	}
+
+        	return tempA;
+        }
+
+    }
+}
