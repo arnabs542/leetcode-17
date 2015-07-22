aec12dd7def0b15a56e5090e3d4ac0ab69fc269a
Dejun Huang <djhuang21@gmail.com>
2015-07-22 17:54:21 -0400

    Add solution for 232 Implement Queue using Stacks

---
+++ 232 Implement Queue using Stacks/Solution.java
@@ -0,0 +1,50 @@
+/*
+Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.
+Notes:
+You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+*/
+
+class MyQueue {
+    private Stack<Integer> main = new Stack<Integer>();
+    private Stack<Integer> helper = new Stack<Integer>();
+
+    // Push element x to the back of queue.
+    public void push(int x) {
+        if (main.empty()) {
+            main.push(x);
+        }
+        else {
+            while (!main.empty()) {
+                int temp = main.pop();
+                helper.push(temp);
+            }
+            main.push(x);
+            while (!helper.empty()) {
+                int temp = helper.pop();
+                main.push(temp);
+            }
+        }
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        main.pop();
+    }
+
+    // Get the front element.
+    public int peek() {
+        return main.peek();
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return main.empty();
+    }
+}
