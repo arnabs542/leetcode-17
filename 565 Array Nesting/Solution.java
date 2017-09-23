44e42721ef935616c1a7f1d5888e6ef7f4af63a1
Dejun Huang <djhuang21@gmail.com>
2017-09-23 12:38:29 -0700

    - add solution for problem 565. Array Nesting

---
+++ 565 Array Nesting/Solution.java
@@ -0,0 +1,36 @@
+// reference: http://www.cnblogs.com/grandyang/p/6932727.html
+class Solution {
+    int[] visitedPos;
+
+    public int arrayNesting(int[] nums) {
+        visitedPos = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) visitedPos[i] = 0;
+
+        int maxSetSize = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int curNum = nums[i];
+            if (visitedPos[curNum] == 0 ) {
+              int curSetSize = this.getCurPosSetSize(i, nums);
+              if (curSetSize > maxSetSize) {
+                  maxSetSize = curSetSize;
+              }
+            }
+        }
+
+        return maxSetSize;
+    }
+
+    private int getCurPosSetSize(int pos, int[] nums) {
+        int curPosSetSize = 0;
+
+        int curPos = pos;
+        while (curPos != pos || curPosSetSize == 0) {
+            visitedPos[curPos] = 1;
+            curPos = nums[curPos];
+            curPosSetSize++;
+        }
+
+        return curPosSetSize;
+    }
+}
