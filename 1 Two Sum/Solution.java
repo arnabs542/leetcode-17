3c9dfb4eb41773c22d0ca0bf0d4b095574f33f3a
Dejun Huang <djhuang21@gmail.com>
2018-12-17 23:28:32 -0800

    - Update and add thoughts for problem 1 Two Sum

--- 1 Two Sum/Solution.java
+++ 1 Two Sum/Solution.java
@@ -1,4 +1,50 @@
 class Solution {
+    // use hash table
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> posMap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(posMap.get(target - nums[i]) != null) {
+                return new int[]{i, posMap.get(target - nums[i])};
+            } else {
+                posMap.put(nums[i], i);
+            }
+        }
+        return new int[2];
+    }
+
+    // use sorting
+    public int[] twoSum_sorting(int[] nums, int target) {
+        Map<Integer, List<Integer>> posMap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            List<Integer> intList;
+            if(posMap.get(nums[i]) == null) {
+                intList = new ArrayList<>();
+                posMap.put(nums[i], intList);
+            } else {
+                intList = posMap.get(nums[i]);
+            }
+            intList.add(i);
+        }
+
+        Arrays.sort(nums);
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(end > start) {
+            int currentSum = nums[start] + nums[end];
+            if(currentSum == target) break;
+            else if(currentSum > target) end--;
+            else start++;
+        }
+
+        if(nums[start] != nums[end]) {
+            return new int[]{posMap.get(nums[start]).get(0), posMap.get(nums[end]).get(0)};
+        } else {
+            return new int[]{posMap.get(nums[start]).get(0), posMap.get(nums[end]).get(1)};
+        }
+    }
+
     class NumWIndex implements Comparable<NumWIndex>{
         public int num;
         public int index;
@@ -14,7 +60,8 @@ class Solution {
         }
     }
 
-    public int[] twoSum(int[] nums, int target) {
+    // original solution
+    public int[] twoSum_original(int[] nums, int target) {
         int[] result = new int[2];
         int len = nums.length;
         NumWIndex[] listOfNums = new NumWIndex[len];
