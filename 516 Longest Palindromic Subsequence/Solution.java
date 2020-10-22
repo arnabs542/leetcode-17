90127fac710f982a60009d69afaece6b22126a5a
Dejun Huang <djhuang21@gmail.com>
2020-10-21 20:18:00 -0700

    Improve solution for problem 516. Longest Palindromic Subsequence

--- 516 Longest Palindromic Subsequence/Solution.java
+++ 516 Longest Palindromic Subsequence/Solution.java
@@ -1,8 +1,10 @@
 class Solution {
     // use DP to solve the problem, we can build recursion expression of the result
     // dp[i][j] means the longest palindrome sub-string from index i to j of the string
-    // dp[i][j] = max(dp[i][j - 1], dp[k][j - 1] + 2)
-    // (k is the first index after the first character that is equal to s[j] between i to j - 1)
+    // if s[i] = s[j], dp[i][j] = dp[i+1][j-1] + 2
+    // otherwise, dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1])
+    // Initialization: dp[i][i] = 1
+    // ref: https://leetcode.com/problems/longest-palindromic-subsequence/discuss/99101/Straight-forward-Java-DP-solution
     public int longestPalindromeSubseq(String s) {
         // special case handling
         if (s == null || s.length() == 0) return 0;
@@ -10,24 +12,15 @@ class Solution {
         int len = s.length();
         int[][] dp = new int[len][len];
 
-        // set default value for dp array
-        for (int i = 0; i < len; i++) {
-            for (int j = i; j < len; j++) {
-                dp[i][j] = 1;
-            }
-        }
-
-        for (int j = 1; j < len; j++) {
-            for (int i = j - 1; i >= 0; i--) {
-                char curChar = s.charAt(j);
-                int k = s.substring(i, j).indexOf(curChar);
-                if (k == -1) {
-                    // cannot find a new pair for current char
-                    if (j > 0) dp[i][j] = dp[i][j - 1];
-                } else {
-                    k += i;
+        for (int i = len - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < len; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
                     // find a new pair for current char to add to palindrome substring
-                    dp[i][j] = Math.max(dp[i][j - 1], dp[k + 1][j - 1] + 2);
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    // cannot find a new pair for current char
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
                 }
             }
         }
